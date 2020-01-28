2b50da844bc7aabb22ce943545b5d8490bdd3fb0
Acxel Morales <acxelmorales97@gmail.com>
2020-01-28 15:38:51 -0600

    Rectangles

--- index.js
+++ index.js
@@ -44,6 +44,18 @@ const rectangles = [
         width: 412,
         height: 60
     },
+    {
+        left: -3,
+        top: 180,
+        width: 582,
+        height: 50
+    },
+    {
+        left: -3,
+        top: 200,
+        width: 500,
+        height: 60
+    }
 ];
 
 // doc.pipe(fs.createWriteStream('tesseract.js-ocr-result.pdf'));
